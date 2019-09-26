b670391a6818b4c1733b699ae0b853e777d3d348
patrickdeyoreo <pdeyoreo@gmail.com>
2019-09-26 12:19:56 -0700

    Committing getln header and source files - provides getln() / fgetln()

---
+++ fgetln.c
@@ -0,0 +1,16 @@
+#include "getln.h"
+
+/**
+ * fgetln - get a line from a file
+ *
+ * @line_p: pointer to the line buffer
+ * @size_p: pointer to the line buffer size
+ * @stream: pointer to the input stream
+ *
+ * Return: If memory allocation fails, return -1.
+ * Otherwise, return the line length (linefeed included).
+ */
+ssize_t fgetln(char **line_p, size_t *size_p, FILE *input)
+{
+	return (getln(line_p, size_p, fileno(input)));
+}
