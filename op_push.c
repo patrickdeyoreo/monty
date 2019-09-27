fa4243f8df007ebc58c424fbadb18e8a5e28b1e2
patrickdeyoreo <pdeyoreo@gmail.com>
2019-09-27 00:04:33 -0700

    Trying strtol isntead of atoi to push ints

--- op_push.c
+++ op_push.c
@@ -8,16 +8,17 @@
 void op_push(stack_t **sp, unsigned int lineno)
 {
 	stack_t *new = NULL;
-	const char *nstr = op_env.argv[1];
+	char *nstr = NULL;
+	int n = strtol(op_env.argv[1], &nstr, 10);
 
-	if (!(nstr && isinteger(nstr)))
+	if (!nstr || *nstr || nstr == op_env.argv[1])
 		failure("L%u: usage: push integer\n", lineno);
 
 	new = malloc(sizeof(*new));
 	if (!new)
 		failure("Error: malloc failed\n");
 
-	new->n = atoi(nstr);
+	new->n = n;
 	if (*sp)
 	{
 		new->prev = (*sp);
